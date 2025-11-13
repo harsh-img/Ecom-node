4c050d22629a613cff2369d7a948392c74dbd2b8
Codethorgod <chirag19.026@gmail.com>
2025-11-13 18:36:56 +0530

    saksham

--- src/controllers/userController/userAccount.controller.js
+++ src/controllers/userController/userAccount.controller.js
@@ -24,3 +24,27 @@ export const userRegister = async (req,res)=>{
         res.status(500).json({message : "Server error in user register",error:error.message})
     }
 }
+
+export const userLogin = async (req, res) => {
+  const { emailOrname, password } = req.body;
+  try {
+    const user = await User.findOne({
+      $or: [{ email: emailOrname }, { name: emailOrname }],
+    });
+    if (!user) {
+      return res.status(400).json({message: "Invalid credentials" });
+    }
+
+    const isMatch = await comparePassword(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials"});
+    }
+    
+    const token = generateToken({ userId: user._id, email: user.email, role: user.role || "customer" });
+    return res.status(200).json({message: "Login successful", user,token });
+
+  } catch (error) {
+    return res.status(500).json({message: "Server error. Please try again.",errors : error.message});}
+};
+
